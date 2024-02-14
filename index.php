fbe1dae623affbf9ae5e0288dda9c7c501a0d39e
david <david@webrelief.fr>
2024-02-14 18:19:59 +0100

    conflit OK

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 echo "index first commit";
+echo "second commit : user 2 modif ligne 4, test conflit";
 echo "second commit : user 2 modif ligne 4";
 echo "index second commit";
 echo "index third commit";
