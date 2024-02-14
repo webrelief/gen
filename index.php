4ab86c5f966aabd6a0f810ba29b8cc26ecc9be07
David <david@webrelief.fr>
2024-02-14 18:29:57 +0100

    message 1

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-
 echo "index first commit";
 echo "second commit : user 2 modif ligne 4, test conflit";
-echo "second commit : user 2 modif ligne 4";
-echo "index second commit";
+echo "modif ici index second commit";
 echo "Modif : index third commit";
 
+echo "ligne ajoutée 2";
+
+
+echo "ligne ajoutée 1";
 echo " - Ajout - ";
