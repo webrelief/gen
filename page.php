d4db96307f210be3afd551a9573479cba39f747d
David <david@webrelief.fr>
2024-02-15 10:06:14 +0100

    conflit resolu

--- page.php
+++ page.php
@@ -1,8 +1,9 @@
 <header>DÉBUT</header>
-
 <h1>Init page</h1>
-<h2>Titre 2</h2>
+<h2>Titre 2 modifié</h2>
+<p>Ajout ICI</p>
 <p>Modif en conflit ICI</p>
+<p>Ajout ICI 2</p>
 
 <footer>Modif pied de page</footer>
 
